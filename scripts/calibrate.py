d387cc04e06e02ad5faad91553a5e221994f4106
Levi <levi.vandekamp@gmail.com>
2020-02-23 00:02:57 -0500

    Updated odrive serial numbers and disabled watchdog in calibrate

--- scripts/calibrate.py
+++ scripts/calibrate.py
@@ -18,7 +18,7 @@ TIMEOUT = 2 # Seconds
 no_calib = False
 
 SERIAL_NUMS = [
-    35593293288011,                  # Left, 0
+    35554687266894	,                  # Left, 0   (old was 35593293288011)
     35623406809166,                  # Middle, 1  (old was 35550393020494)
     35563278839886]                  # Right, 2
 
@@ -75,7 +75,7 @@ def set_params(ax):
     ax.controller.vel_ramp_enable = True
 
     # # Unset watchdog
-    # ax.config.watchdog_timeout = 0
+    ax.config.watchdog_timeout = 0
 
 
 def wait_and_exit_on_error(ax):
